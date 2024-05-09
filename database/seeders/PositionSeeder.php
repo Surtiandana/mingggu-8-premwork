653e1aa8178766db58b94ca978cd0071f4741742
firohazzah <dhofiroh@student.telkomuniversity.ac.id>
2024-05-09 12:01:19 +0700

    first commit

---
+++ database/seeders/PositionSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PositionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        sition::factory()->count(5)->create();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+// Rute untuk halaman home
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+// Rute untuk halaman profile
+Route::get('profile', ProfileController::class)->name('profile');
+
+// Rute untuk resource employees
+Route::resource('employees', EmployeeController::class);
+
+// Rute untuk halaman welcome
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
